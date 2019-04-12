e774beb61c1ffa5a2ff4b1f5380c4d653064f97c
afeiship <1290657123@qq.com>
2019-04-12 14:25:25 +0800

    fix: desc replace

--- generators/app/index.js
+++ generators/app/index.js
@@ -31,7 +31,6 @@ module.exports = class extends Generator {
         name: 'description',
         message: 'Your description?'
       },
-      ,
       {
         type: 'input',
         name: 'namespace',
@@ -67,6 +66,8 @@ module.exports = class extends Generator {
   end() {
     const { project_name, description, namespace } = this.props;
     const files = glob.sync(resolve(this.destinationPath(), '{**,.*}'));
+
+    console.log('files:', files)
 
     replace.sync({
       files,
